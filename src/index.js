ec2ac200f1ecca8f08c586e2f866ecc720d786c9
VitalyMikulich <mr.wital1999@gmail.com>
2019-03-24 20:54:33 +0300

    solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let maxFactor;
+  let factors = [];
+  let baseCopy = base;
+  for(let i = 2; i <= base; i++) {
+    let count = 0;
+    while(baseCopy % i == 0) {
+      baseCopy /= i;
+      count++;
+    }
+    if(count != 0) {
+      factors[i] = count;
+      maxFactor = i;
+    }
+  }
+  let result = 0;
+  while(number > 0) {
+    number = Math.floor(number /= maxFactor);
+    result += number;
+  }
+
+  return result;
 }
